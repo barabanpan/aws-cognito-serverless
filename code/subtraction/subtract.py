9368ddec8d84560b2a8b38f3099c82c1ecc5e481
Nataliia Dyshko <nataliia.dyshko@jetsoftpro.com>
2020-11-03 13:14:52 +0200

    Add log_request decorator to all lambdas

--- code/subtraction/subtract.py
+++ code/subtraction/subtract.py
@@ -1,9 +1,14 @@
 import json
 
+from decorators import log_request
+
+
+@log_request
 def subtract(event, context):
     args = json.loads(event['body'])
     a, b = args.get('a'), args.get('b')
     res = eval(f'{a} - {b}')
+
     return {
         'statusCode': 200,
         'body': json.dumps({'res': res})
