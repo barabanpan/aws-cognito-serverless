99f8a01c5679f46ce09730b743c5bb25af9d6536
Nataliia Dyshko <nataliia.dyshko@jetsoftpro.com>
2020-11-03 12:27:57 +0200

    Fix small bugs

--- code/subtraction/subtract.py
+++ code/subtraction/subtract.py
@@ -3,9 +3,9 @@ import json
 def subtract(event, context):
     args = json.loads(event['body'])
     a, b = args.get('a'), args.get('b')
-    res = a - b
+    res = eval(f'{a} - {b}')
     return {
         'statusCode': 200,
-        'body': json.dumps({"res": res})
+        'body': json.dumps({'res': res})
     }
 
