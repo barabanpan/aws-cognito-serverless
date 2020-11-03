030fbacd4a5aad469a3455c89b500fbee0c6f7f3
Nataliia Dyshko <nataliia.dyshko@jetsoftpro.com>
2020-11-03 13:14:14 +0200

    Add a layer with a decorator to log requests to dynamodb

---
+++ code/layers/basic_layer/python/decorators.py
@@ -0,0 +1,15 @@
+from dynamodb import DatabaseManager
+
+
+def log_request(func):
+    def wrapper(event, context):
+        request_body = event['body']
+        result = func(event, context)
+        
+        # log request
+        db = DatabaseManager()
+        db.add_new_log(request_body, result['body']['res'])
+        
+        return result
+    return wrapper
+
