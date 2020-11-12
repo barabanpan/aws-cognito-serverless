3dfede913df207a9886afde58582804380471f71
Nataliia Dyshko <nataliia.dyshko@jetsoftpro.com>
2020-11-12 19:14:17 +0200

    Add cognito.py

---
+++ code/layers/basic_layer/python/cognito.py
@@ -0,0 +1,11 @@
+import base64
+import hashlib
+import hmac
+
+from bq_constants import CLIENT_ID, CLIENT_SECRET
+
+
+def get_secret_hash(username):
+    msg = username + CLIENT_ID
+    dig = hmac.new(str(CLIENT_SECRET).encode('utf-8'), msg=str(msg).encode('utf-8'), digestmod=hashlib.sha256).digest()
+    return base64.b64encode(dig).decode()
