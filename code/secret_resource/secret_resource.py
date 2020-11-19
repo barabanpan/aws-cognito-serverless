75e172453aab252312acf923a5ddef6e8611c19e
Nataliia Dyshko <nataliia.dyshko@jetsoftpro.com>
2020-11-19 19:13:01 +0200

    Add a secret resource lambda (for logged in)

---
+++ code/secret_resource/secret_resource.py
@@ -0,0 +1,8 @@
+import json
+
+
+def handler(event, context):
+    return {
+        'statusCode': 200,
+        'body': json.dumps({'message': "Hi! That's a secret resource! The answer is 42."})
+    }
