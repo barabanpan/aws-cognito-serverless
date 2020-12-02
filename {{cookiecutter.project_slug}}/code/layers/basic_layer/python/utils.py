21e486a9ba2b9989da05024baf5d0d03e9b2ebd1
Serhii Siuziev <snake_wizard@protonmail.com>
2020-12-02 19:40:23 +0200

    #9: Add cors headers

--- {{cookiecutter.project_slug}}/code/layers/basic_layer/python/utils.py
+++ {{cookiecutter.project_slug}}/code/layers/basic_layer/python/utils.py
@@ -5,14 +5,17 @@ from constants import DEFAULT_IS_GOOD_BOY, DATE_FORMAT
 
 
 def response(code, body):
-    return {"statusCode": code,
-            "headers": {
-                "X-Requested-With": "*",
-                "Access-Control-Allow-Headers": "*",
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "*"
-            },
-            "body": json.dumps(body)}
+    return {
+        "statusCode": code,
+        "headers": {
+            "X-Requested-With": "*",
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "*",
+            "Access-Control-Allow-Credentials": "true"
+        },
+        "body": json.dumps(body)
+    }
 
 
 def bad_request(message):
