2537d40fc85bfcb01caf2e43fd72c003ea5363d9
Serhii Siuziev <snake_wizard@protonmail.com>
2020-12-04 14:40:02 +0200

    Add fabfile with console commands shortcuts

---
+++ {{cookiecutter.project_slug}}/fabfile.py
@@ -0,0 +1,23 @@
+"""
+Please install fabric3 before run command from this module.
+Install fabric3 from console:
+>>> python3 -m pip install -U pip
+>>> python3 -m pip install -U fabric3
+"""
+
+
+from fabric.api import local, task
+
+
+@task
+def sls_install():
+    """Install serverless requirements."""
+    local("npm install -g serverless")
+    local("sls plugin install -n serverless-s3-sync")
+    local("sls plugin install -n serverless-python-requirements")
+
+
+@task
+def sls_deploy(stage="dev"):
+    """Deploy project on AWS with Serverless framework."""
+    local(f"sls deploy --stage {stage}")
