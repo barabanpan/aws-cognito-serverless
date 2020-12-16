080d234193d335b23aef59aaacbc2f93ed50bf14
Nataliia Dyshko <nataliia.dyshko@jetsoftpro.com>
2020-12-16 23:18:55 +0200

    #41 Rewrite write_to_bucket to use csv instead of txt

--- {{cookiecutter.project_slug}}/code/write_to_bucket/write_to_bucket.py
+++ {{cookiecutter.project_slug}}/code/write_to_bucket/write_to_bucket.py
@@ -1,4 +1,6 @@
 import boto3
+import codecs
+import csv
 from datetime import datetime
 import json
 import os
@@ -9,32 +11,52 @@ from utils import bad_request, response
 
 WRITE_TO_BUCKET_NAME = os.environ.get("WRITE_TO_BUCKET_NAME")
 s3 = boto3.client("s3")
+delimiter = ";"
+
+
+class Pipe:
+    """Helper class for writing csv to a bucket."""
+    value = ""
+
+    def write(self, text):
+        self.value = self.value + text
 
 
 def handler(event, context):
     try:
         entry = json.loads(event["body"])["entry"]
-    except Exception as e:
-        return bad_request("There is no entry in request body!")
+    except Exception:
+        return bad_request("There is no 'entry' in request body!")
 
+    # read
+    rows = list()
     try:
         obj = s3.get_object(Bucket=WRITE_TO_BUCKET_NAME, Key=FILE_NAME)
-        body = obj["Body"].read().decode("utf-8") + "\n\n"  # add chunks in case of a big file?
+        body = csv.DictReader(codecs.getreader("utf-8")(obj["Body"]), delimiter=delimiter)
+        for i, row in enumerate(body):  # add chunks in case of a big file?
+            rows.append(dict(row))
     except (s3.exceptions.NoSuchKey, KeyError):
-        body = ""
+        pass
     except s3.exceptions.NoSuchBucket:
         return bad_request("No bucket with name " + WRITE_TO_BUCKET_NAME + "!")
 
+    # add new
+    next_id = 1 + max([int(row["id"]) for row in rows] or [0])
     date = datetime.now().strftime("%d.%m.%Y %H:%M")
-    body += date + "\n" + entry
+    rows.append({
+        "id": str(next_id),
+        "created_at": date,
+        "text": entry
+    })
 
-    s3.put_object(Body=str.encode(body), Bucket=WRITE_TO_BUCKET_NAME, Key=FILE_NAME)
+    # write
+    pipe = Pipe()
+    writer = csv.DictWriter(pipe, ["id", "created_at", "text"], delimiter=delimiter)
+    writer.writeheader()
+    for row in rows:
+        writer.writerow(row)
 
-    # return last 10 records from file
-    body = body.split("\n")
-    last_records = []
-    for i, record in enumerate(body[-10:]):
-        last_records.append({"id": i + 1, "text": record})
+    s3.put_object(Body=str.encode(pipe.value), Bucket=WRITE_TO_BUCKET_NAME, Key=FILE_NAME)
 
     return response(200, {"message": "New entry was added",
-                          "last_records": last_records})
+                          "last_records": rows[-10:]})
