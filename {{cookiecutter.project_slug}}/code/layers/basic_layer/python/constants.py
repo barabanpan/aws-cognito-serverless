c741f85058c3abe42601a1fecc3d91537612ae0b
Nataliia Dyshko <nataliia.dyshko@jetsoftpro.com>
2020-12-19 16:52:44 +0200

    #74 Make 'reader' group a default group at sign_up

--- {{cookiecutter.project_slug}}/code/layers/basic_layer/python/constants.py
+++ {{cookiecutter.project_slug}}/code/layers/basic_layer/python/constants.py
@@ -8,3 +8,6 @@ FILE_NAME = "{{cookiecutter.s3_file_name}}.csv"
 
 # list of available cognito groups for users
 COGNITO_USER_POOL_GROUPS = ["reader", "modifier"]
+
+# default group for all users
+COGNITO_DEFAULT_GROUP = ["reader"]
